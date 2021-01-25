f2d15e38766e47a7ee705c8d171bab9931a5bb75
mastercookie29 <mastercookie29@icloud.com>
2021-01-25 18:05:17 +0000

    written in Visual Studio code/ copy and pasted from there.

---
+++ assignment 2-1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+    int checking;
+    unsigned int miles;
+    long days;
+
+    checking = -20;
+    miiles = 4276;
+    days = 189000;
+    cout << "We have made a long journey of " ,,miles;
+    cout << " miles.\n";
+    cout << "Our checking account balance is " << checking;
+    cout << "\nAbout" << days << " days abog Columbus ";
+    cout << "stood on this spot. \n";
+    return 0;
+}
