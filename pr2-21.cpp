8ac82633271e6d4eae10c57d1aed07bff4f39427
mastercookie29 <mastercookie29@icloud.com>
2021-01-27 17:33:54 +0000

    Assignemnts 2-21,2-22,2-23 were added.

---
+++ pr2-21.cpp
@@ -0,0 +1,37 @@
+// Write a program that calculates and displays an employee’s total wages for the week.
+
+// The regular hours for the work week are 40,
+// and any hours worked over 40 are considered overtime.
+// The employee earns $18.25 per hour for regular hours and $27.78 per hour for overtime hours.
+// The employee has worked 50 hours this week.
+ 
+
+// Make the pseudocode algorithm shows the program’s logic.
+
+// Pseudocode
+
+// Regular wages = base pay rate × regular hours
+// Overtime wages = overtime pay rate × overtime hours
+// Total wages = regular wages + overtime wages
+// Display the total wages
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  double work_hours=50;
+  double regular_wages, overtime_wages;
+  double base_pay_rate=18.25, regular_hours=40, overtime_pay_rate= 27.78, overtime_hours=10;
+  double total_wages;
+
+  regular_wages = regular_hours * base_pay_rate;
+  overtime_hours = work_hours - regular_hours;
+  overtime_wages = overtime_hours * overtime_pay_rate
+  total_wages = regular_wages + overtime_wages;
+
+  cout << "Regular wages " << regular_wages << endl;
+  cout << "Overtime wages " << overtme_wages << endl;
+  cout << "Total wages " << total_wages << endl;
+
+}
---
+++ pr2-22.cpp
@@ -0,0 +1,28 @@
+# include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int money_earned = 6000;
+  double percent1 = 0.05;
+  double percent2 = 0.07;
+  double percent3 = 0.1;
+
+  double into_savings1;
+  double into_savings2;
+  double into_savings3;
+
+  into_savings1 = money_earned * percent1 
+  into_savings2 = money_earned * percent2 
+  into_savings3 = momey_erend * percent3 
+
+  cout << "If you put 5% in savings you will put " <<into_savings1<< endl;
+
+  cout << "If you put 7% in savings you will put " << into_savings2<< endl;
+
+  cout << "If you put 10% in savings, you will put " << into_savings3 << endl;
+
+  return 0;
+  g+
+}
