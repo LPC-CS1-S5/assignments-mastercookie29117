c88683ed0226caad392c60c2b7a8248f2bb497d5
mastercookie29 <mastercookie29@icloud.com>
2021-02-03 18:05:51 +0000

    added assignment 3-4 into folder chapter 3

---
+++ chapter 3 assignments/assignment3-4.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int month_1
+  int month_2
+  int month_3
+
+  float mon1_rain
+  float mon2_rain
+  float mon3_rain
+
+  cout << "What is the first month? " << endl;
+  cin >> month_1 
+  cout << "What is your second month?" << endl;
+  cin >> month_2
+  cout << "What is the thrid and final month" << endl;
+  cin >> month_3
+
+  cout << "How much rainfall did month 1 have? " << endl;
+  cin >> mon1_rain
+
+  count << "How much rainfall did month 2 have?" << endl; 
+  cin>> mon2_rain
+
+  cout<< "How much rainfall did month 3 have? " << endl;
+  cin >> mon3_rain
+
+  total_rain = mon1_rain + mon2_rain + mon3_rain / 3 
+
+  cout << "The average rainfall for " << month_1 << " " << month_2 << " " << month_3 << " is " << total_rain << ". " << endl;
+
+  return 0;
+}
