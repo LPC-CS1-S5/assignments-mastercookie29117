3f9293f85c1ee48a7c9fa7738a1a19485118c90a
mastercookie29 <mastercookie29@icloud.com>
2021-02-10 02:54:08 +0000

    added assignment 3-1 to chapter 3 folder

---
+++ chapter 3 assignments/assingment3-1.cpp
@@ -0,0 +1,46 @@
+// created by Joshua Stallard
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  float cl_a, cl_b,cl_c
+  double price_a = 15
+  double price_b = 12
+  double price_c = 9
+
+
+  cout << "How many tickets were sold for game A" << endl;
+  cin >> cl_a;
+
+  // calculate the cost of the tickets for the games.
+  finprice_a = cl_a * price_a
+
+  cout << "The final price of the tickets is " << finprice_a << " ." << endl;
+  
+  //second price of the ticket sales.
+
+  cout << "How many tickets were sold for game B" << endl;
+
+  cin >> cl_b;
+
+  finprice_b = cl_b * price_b;
+
+  cout << "The final price of the tickets for game 2 is " << finprice_b << " ." << endl;
+
+  // calculate the cost of the tickets from the game 3
+
+  cout << "How many tickets were sold for gmae 3?" << endl;
+  cin >> cla_c
+
+  finprice_c = cl_c * price_c;
+
+  cout << "The final price of the tickets is " << finprice_c << " ." << endl; 
+
+  return 0; 
+
+
+}
