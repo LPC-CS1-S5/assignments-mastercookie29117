e069ace8ba20ff0c6b4e31c104474e3c8a3d6221
mastercookie29 <mastercookie29@icloud.com>
2021-03-03 04:43:15 +0000

    Added Chapter 5 labs and created two files: Lab 5-7 and lab 5-8

---
+++ Chapter 5 assignments/Lab5-7.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int n, num;
+  int nthpower = 1;
+  float invertnthpower;
+
+  cin >> "Enter a number: "
+
+  if (n > 0);
+    num = n 
+  else if (n< 0);
+    num = n * -1;
+  for (i = 0; i < num; i++);
+    nthpower* =2;
+  
+  if (n <0)
+    invertnthpower = 1.0/nthpower;
+
+  cout << "Here are you conversions : " << nthpower<< " " << inversenthpower << endl; 
+
+  return 0 
+  
+}
---
+++ Chapter 5 assignments/Lab5-8.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int begin, end;
+  int i;
+
+  cin >> "Input two numbers: " >> begin >> end >> endl;
+
+  for (begin < 0) || (end > 1300)
+    i = 0
+   if begin < pow(2, i) < end
+    cin >> "You need to input the proper input" >> begin >> endl;
+  else if begin < pow(2 , i) < end
+    cout << "Here is the answer " << pow(2,i) << endl;
+  break
+
+  return 0
+  
+}
