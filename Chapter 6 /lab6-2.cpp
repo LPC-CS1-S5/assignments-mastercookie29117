39e094a03c392f792d75d13f1c5aadf3c73e0c9e
mastercookie29 <mastercookie29@icloud.com>
2021-03-23 21:15:24 +0000

    added chapter 6 lab assignments 6-1, 6-2

---
+++ Chapter 6 /lab6-2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  double nthpowern(int n) {
+    int i;
+    double nthpower = 1/2;
+    double result; 
+    for (i=0; i<n; i++)
+    {
+      result *= nthpower;
+    }
+  }
+  void powernumbers(int base, int exponent)
+  {
+    int i; 
+    int nthpower=1;
+    for (i =1; i<=exponent; i++)
+    {
+      nthpower *= base;
+      cout << "Power of " << i << nthPower << endl;
+    }
+  }
+
+  cout << "Enter in the number you would like to convert: " << endl;
+  cin << n << endl;
+
+ 
+}
