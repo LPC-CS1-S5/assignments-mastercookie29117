b326c53290fd6138375fbb1518476d1e7fadcc94
Joshua Stallard <jstallard117@gmail.com>
2021-03-31 16:15:04 +0000

    added question 3 file to folder

---
+++ Midterm/midtermQ3.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+#include <time.h>
+
+using namespace std;
+
+int getrdnum(void)
+
+int main()
+{
+  int N;
+
+  n = rand() % 1 + 50;
+
+
+}
+
+int getrdnum()
+{
+  int N;
+
+  n = rand() % 1 + 50;
+
+}
+
+int isgreater(num)
+{
+  
+}
