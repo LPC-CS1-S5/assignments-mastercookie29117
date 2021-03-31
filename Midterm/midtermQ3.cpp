9b060f3af524dba4812db200460892e928ac9acd
Joshua Stallard <jstallard117@gmail.com>
2021-03-31 16:26:57 +0000

    finished q3 random number generator

--- Midterm/midtermQ3.cpp
+++ Midterm/midtermQ3.cpp
@@ -9,9 +9,8 @@ int getrdnum(void)
 
 int main()
 {
-  int N;
-
-  n = rand() % 1 + 50;
+  getrdnum();
+  isgreater();
 
 
 }
@@ -20,11 +19,23 @@ int getrdnum()
 {
   int N;
 
-  n = rand() % 1 + 50;
+  n = rand() % 50;
+  for (int N = 0; N < 0; N++) {
+    cout << "The random number is: " << rand()%N << endl;
+
+  }
 
 }
 
-int isgreater(num)
+int isgreater(int n)
 {
+  if (n = n)
+    break
+  if (N > 1) 
+    cout<< getrdnum << " is your list of numbers"
+  if (N <1)
+    break
+  else 
+    cout<< "Random generator failed"
   
 }
