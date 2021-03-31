1d4505cf8f4c1fc9ec5bf00461b56f89eaec760c
Joshua Stallard <jstallard117@gmail.com>
2021-03-31 15:27:33 +0000

    made changes to q2 in midterm

---
+++ Midterm/midtermQ2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int maim()
+{
+  ofstream outputFile;
+  outputFile.open("students.txt")
+  int name1, name2, name3, name4, name5, name6, name7, name8,name9, name10;
+  double score1, score2, score3, score4, score5, score6, score7, score8, score9, score10,score11, score12, score13, score14, score15, score16, score17, score18, score19, score20;
+
+
+  //Write each students records.
+
+  outputFile << "James
+
+}
